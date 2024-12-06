4f41c813629f8cda58a6dea2156307798778b9cb
devaapriana <deevaa.apriana@gmail.com>
2024-12-06 16:52:20 +0700

    feat: apply middleware auth api

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,10 @@ Route::group([
     'middleware' => 'api',
 ], function () {
     Route::post('login', [AuthController::class, 'login']);
-    Route::get('me', [AuthController::class, 'me']);
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::post('refresh', [AuthController::class, 'refresh']);
+
+    Route::middleware('auth:api')->group(function () {
+        Route::post('me', [AuthController::class, 'me']);
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
+    });
 });
